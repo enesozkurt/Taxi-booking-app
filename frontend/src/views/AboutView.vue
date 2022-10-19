68a5332dfe25c9e9c1e7f9c6feda3b18a298011e
Enes Özkurt <enes.ozkurt4@gmail.com>
2022-10-19 13:28:42 -0700

    html elements convert to pug

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,5 +1,16 @@
-<template>
-  <div class="about">
+<script>
+import HelloWorld from '@/components/HelloWorld'
+
+export default {
+  name: 'AboutView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<template lang="pug">
+.about
     <h1>This is an about page</h1>
-  </div>
+    HelloWorld(msg="Hello world component")
 </template>
